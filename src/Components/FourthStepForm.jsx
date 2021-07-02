16d039e2a8b3dc1d3aa21ac604876b4b3b6e2171
Egzon Berisha <egzonberisha232@gmail.com>
2021-07-02 14:32:41 +0200

    form validation donne, icons added for each course

--- src/Components/FourthStepForm.jsx
+++ src/Components/FourthStepForm.jsx
@@ -5,16 +5,19 @@ const FourthStepForm = (Props) => {
     const SetFormStep = () => {setSteps(Steps - 1)}
 
     return (
-        <form className="fourth_page">
+        <form className="fourth_page" onSubmit={handleSubmit}>
             <h3>Veuillez remplir les informations, un conseiller vous recontactera prochainement :</h3>
-            <input required type="text" placeholder="Votre Prénom" onChange={(e) => setName(e.target.value)} /><br/>
-            <input required type="text" placeholder="Votre Nom" onChange={(e) => setFamillyName(e.target.value)} /><br/>
-            <input required type="text" placeholder="Votre Email" onChange={(e) => setEmail(e.target.value)} /><br/>
-            <input required type="number" placeholder="Votre Téléphone" onChange={(e) => setPhoneNumber(e.target.value)} /><br />
+            <input type="text" placeholder="Votre Prénom" onChange={(e) => setName(e.target.value)}  required/><br/>
+            <input type="text" placeholder="Votre Nom" onChange={(e) => setFamillyName(e.target.value)} required/><br/>
+            <input type="email" placeholder="Votre Email" onChange={(e) => setEmail(e.target.value)} required/><br/>
+            <input type="number" placeholder="Votre Téléphone" onChange={(e) => setPhoneNumber(e.target.value)} required/><br />
             <Button variant="contained" color="primary" onClick={SetFormStep}>RETOUR</Button>
-            <Button variant="contained" color="primary" onClick={handleSubmit} >VALIDER</Button>
+            <Button variant="contained" color="primary" type="submit">VALIDER</Button>
         </form>
     )
 }
 
 export default FourthStepForm
+
+
+
--- src/Components/ThirdStepForm.jsx
+++ src/Components/ThirdStepForm.jsx
@@ -1,10 +1,18 @@
-import { useState } from "react"
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import Button from '@material-ui/core/Button';
+import Englaandflag from "../Images/england.png"
+import GermannyFlag from "../Images/germanny.png"
+import SpainFlag from "../Images/spain.png"
+import ItalyFlag from "../Images/italy.png"
+import Word from "../Images/Word.png"
+import Outlook from "../Images/outlook.jpg"
+import Excel from "../Images/exel.png"
+import Powerpoint from "../Images/powerpoint.png"
+import Office365 from "../Images/packoffice.png"
 
 
     const ThirdStep = (Props) => {
@@ -16,6 +24,16 @@ import Button from '@material-ui/core/Button';
             setValue(event.target.value);
         }
 
+        const englandFLAG = <span className='flags'><img src={Englaandflag} alt="Anglais" /> Anglais</span>
+        const germnannyFLAG = <span className='flags'><img src={GermannyFlag} alt="Anglais" /> Allemand</span>
+        const spainFLAG = <span className='flags'><img src={SpainFlag} alt="Anglais" /> Espagnol</span>
+        const italyFLAG = <span className='flags'><img src={ItalyFlag} alt="Anglais" /> Italien</span>
+        const wordICON = <span className='flags'><img src={Word} alt="Anglais" /> Word</span>
+        const outlookICON = <span className='flags'><img src={Outlook} alt="Anglais" /> Outlook</span>
+        const excelICON = <span className='flags'><img src={Excel} alt="Anglais" /> Excel</span>
+        const powerpointICON = <span className='flags'><img src={Powerpoint} alt="Anglais" /> Powerpoint</span>
+        const office365ICON = <span className='flags'><img src={Office365} alt="Anglais" />  Pack office 365</span>
+
         return (
             <>
                 <h3>Sélectionnez la formation que vous souhaitez :</h3>
@@ -24,10 +42,10 @@ import Button from '@material-ui/core/Button';
                         <FormControl component="fieldset">
                             <FormLabel component="legend">Langues :</FormLabel>
                             <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
-                                <FormControlLabel value="🇬🇧 Anglais" control={<Radio />} label="🇬🇧 Anglais" />
-                                <FormControlLabel value="🇩🇪 Allemand" control={<Radio />} label="🇩🇪 Allemand" />
-                                <FormControlLabel value="🇪🇸 Espagnol" control={<Radio />} label="🇪🇸 Espagnol" />
-                                <FormControlLabel value="🇮🇹 Italien" control={<Radio />} label="🇮🇹 Italien" />
+                                <FormControlLabel value="🇬🇧 Anglais" control={<Radio />} label={englandFLAG}/>
+                                <FormControlLabel value="🇩🇪 Allemand" control={<Radio />} label={germnannyFLAG} />
+                                <FormControlLabel value="🇪🇸 Espagnol" control={<Radio />} label={spainFLAG} />
+                                <FormControlLabel value="🇮🇹 Italien" control={<Radio />} label={italyFLAG} />
                                 <FormControlLabel value="Autres langues" control={<Radio />} label="Autres langues" />
                             </RadioGroup>
                         </FormControl>
@@ -37,11 +55,11 @@ import Button from '@material-ui/core/Button';
                         <FormControl component="fieldset">
                             <FormLabel component="legend">Bureautique :</FormLabel>
                             <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
-                                <FormControlLabel value="Word" control={<Radio />} label="Word" />
-                                <FormControlLabel value=" Outlook" control={<Radio />} label=" Outlook" />
-                                <FormControlLabel value="Excel" control={<Radio />} label="Excel" />
-                                <FormControlLabel value="Powerpoint" control={<Radio />} label="Powerpoint" />
-                                <FormControlLabel value=" Pack office 365" control={<Radio />} label=" Pack office 365" />
+                                <FormControlLabel value="Word" control={<Radio />} label={wordICON} />
+                                <FormControlLabel value=" Outlook" control={<Radio />} label={outlookICON} />
+                                <FormControlLabel value="Excel" control={<Radio />} label={excelICON} />
+                                <FormControlLabel value="Powerpoint" control={<Radio />} label={powerpointICON} />
+                                <FormControlLabel value=" Pack office 365" control={<Radio />} label={office365ICON} />
                             </RadioGroup>
                         </FormControl>
                     </div>
