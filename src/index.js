d641288748436237b12174a2bab886c383e73968
Egzon Berisha <egzonberisha232@gmail.com>
2021-07-05 16:18:08 +0200

    react router added again

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../src/Style/index.css';
 import App from '../src/Components/App';
+import { HashRouter } from "react-router-dom"
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter><App /></HashRouter>,
   </React.StrictMode>,
   document.getElementById('root')
 );
